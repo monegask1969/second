efc9da9cd538cede55ae5c573f095ae3a936acc0
Макс_с_пары_джавы <monegask1969@gmail.com>
2015-12-27 10:07:26 +0200

    Операции со строками

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       String strInt="123";
+        String strDouble="123.456";
+        int x; double y;
+        x= Integer.parseInt(strInt);
+        y=Double.parseDouble(strDouble);
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        strInt=String.valueOf(x+1);
+        strDouble=String.valueOf(y+1);
+        System.out.println("strInt="+strInt);
+        System.out.println("strDouble="+strDouble);
+        String str;
+        str="num="+345;
+        System.out.println(str);
+    }
+}
+
