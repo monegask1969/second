0cb5244f08e365a1a52c68039ebfdedcfcc3aaa1
Макс_с_пары_джавы <monegask1969@gmail.com>
2016-01-16 12:29:12 +0200

    работа с массивами инвертация

---
+++ src/com/company/Box.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by uitschool JV on 1/16/2016.
+ */
+public class Box {
+    double width;
+    double height;
+    double depth;
+
+    void volume(){
+        System.out.println("Volume is ");
+        System.out.println(width * height * depth);
+    }
+}
