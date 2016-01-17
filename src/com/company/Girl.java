4e2f4a151db63b3a98205c3760b770f64bd48c20
Макс_с_пары_джавы <monegask1969@gmail.com>
2016-01-17 10:51:58 +0200

    Конструктор

---
+++ src/com/company/Girl.java
@@ -0,0 +1,49 @@
+package com.company;
+
+/**
+ * Created by uitschool JV on 1/17/2016.
+ */
+public class Girl {
+    int age;
+    int weight;
+    boolean pretty;
+    boolean acception;
+
+    Girl() {
+        age = 23;
+        weight = 56;
+        pretty = true;
+        acception = true;
+    }
+
+    Girl(int age, int weight, boolean pretty) {
+        this.age = age;
+        this.weight = weight;
+        this.pretty = pretty;
+        acception = false;
+    }
+
+    String cinema(String cinema) {
+        if (cinema.equals("Horror") && acception == false) {
+            return "We will not go!";
+        } else {
+            return "Ok, I will go!";
+        }
+    }
+
+    String restaurant(String restaurant) {
+        if (restaurant.equals("McDonalds") && acception == false) {
+            return "We will not go!";
+        } else {
+            return "Ok, I will go!";
+        }
+    }
+
+    void Info()
+    {
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+        System.out.println("Pretty: " + pretty);
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,47 @@
 package com.company;
 
-import java.util.Arrays;
-
-import static com.company.ArrayInvert.invert;
-
 
 public class Main {
 
     public static void main(String[] args) {
-        int[]a={1,2,3,4,5,6,7};
-        System.out.println(Arrays.toString(a));
-        invert(a);
-        System.out.println(Arrays.toString(a));
+       /* Box mybox1 = new Box(10,20,15);
+        Box mybox2 = new Box();
+        Box mybox3 = new Box(12.25);
+
+        System.out.println(mybox1);
+        System.out.println(mybox2);
+        System.out.println(mybox3);*/
+
+       /* Test ob = new Test(15,20);
+
+        int x=10; int y=10;
+        ob.meth(x,y);
+        System.out.println("x and y before calll: " + x + " " + y);
+        System.out.println("o.a and o.b before call: " + ob.a + " " + ob.b);
+        ob.meth(ob);            //передача ссылки
+        System.out.println("ob.a and ob.b after call: " + ob.a + " " + ob.b);*/
+
+        Girl Katya = new Girl(17,88,false);
+        Girl Masha = new Girl();
+        Girl Alina = new Girl(20,52,true);
+
+        String cinema = "Horror";         // 0 - ужасы
+        String restaurant = "McDonalds";     // 0 - мак
+
+        System.out.println("Info about Katya:");
+        Katya.Info();
+        System.out.println("Katya say about cinema: " + Katya.cinema(cinema));
+        System.out.println("Katya say about restaurant: " + Katya.restaurant(restaurant));
+        System.out.println();
+        System.out.println("Info about Masha:");
+        Masha.Info();
+        System.out.println("Masha say about cinema: " + Masha.cinema(cinema));
+        System.out.println("Masha say about restaurant: " + Masha.restaurant(restaurant));
+        System.out.println();
+        System.out.println("Info about Alina:");
+        Alina.Info();
+        System.out.println("Alina say about cinema: " + Alina.cinema(cinema));
+        System.out.println("Alina say about restaurant: " + Alina.restaurant(restaurant));
 
     }
 
